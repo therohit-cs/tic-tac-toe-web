681d146dfc393e2903bcd83c5f2a2508af3c6bb6
therohit-cs <rohithjithendran@gmail.com>
2025-06-26 13:12:41 +0530

    Implement game logic: player turns and win detection

--- app.js
+++ app.js
@@ -25,6 +25,23 @@ boxes.forEach((box) => {
             box.innerText="X";
         }
         box.disabled=true;
+
+        checkWinner();
     });
 });
+
+
+const checkWinner = () =>{
+    for( let pattern of winPatterns) {
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if(pos1val != "" && pos2val != "" && pos3val != "") {
+            if(pos1val === pos2val && pos2val === pos3val) {
+                console.log("winner",pos1val);
+            }
+        }
+    }
+};
 
